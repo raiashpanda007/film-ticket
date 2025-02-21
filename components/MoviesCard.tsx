ed36fd34cdbda46180a8201685bc9469b91bb991
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-02-21 14:23:16 +0530

    Created Movies card

--- components/MoviesCard.tsx
+++ components/MoviesCard.tsx
@@ -1,8 +1,15 @@
+"use client"
 import React from 'react'
-
+import { useRouter } from 'next/router'
 function MoviesCard({img,name,id}:{img:string,name:string,id:number}) {
+    const router = useRouter()
   return (
-    <div></div>
+    <div className='h-28 w-36 rounded-md' onClick={()=>router.push(`/movies/${id}`)}>
+        <img src={img} alt="" />
+        <div className='text-center'>
+            <h1>{name}</h1>
+        </div>
+    </div>
   )
 }
 
